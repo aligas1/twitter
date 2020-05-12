cd82659d71d0adad0f33183e0760a9c153e7692b
aligas1 <alaligas@gmail.com>
2020-05-11 23:22:22 -0400

    adjustments to Spark consumer. Ran mini batch and wrote to hdfs for schema inference 5/11

--- intelliJ/covidConsumer/build.sbt
+++ intelliJ/covidConsumer/build.sbt
@@ -16,8 +16,17 @@ libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.4"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
 libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.4"
 
+// https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.3.1"
+
+//// https://mvnrepository.com/artifact/org.apache.kafka/kafka-streams
+libraryDependencies += "org.apache.kafka" % "kafka-streams" % "2.4.0"
+
 // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
 libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.12.1"
 
-// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+// https://mvnrepository.com/artifact/org.apache.kafka/kafka
+libraryDependencies += "org.apache.kafka" %% "kafka" % "2.4.1"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
+libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.4"
--- intelliJ/covidConsumer/src/main/scala/covidTwitter/covidConV1.scala
+++ intelliJ/covidConsumer/src/main/scala/covidTwitter/covidConV1.scala
@@ -3,31 +3,44 @@ package covidTwitter
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.types._
+
 
 object covidConV1 {
 
-  // set logger to show errors
-  val rootLogger = Logger.getLogger("org")
-  rootLogger.setLevel(Level.ERROR)
+  def main(args: Array[String]): Unit = {
+    // set logger to show errors
+    val rootLogger = Logger.getLogger("org")
+    rootLogger.setLevel(Level.ERROR)
 
-  // set spark configuration
-  val conf = new SparkConf().setAppName("covidConsumer").setMaster("local[*]")
-  // enable hbase support??
+    // set spark configuration
+    val conf = new SparkConf().setAppName("covidConsumer").setMaster("local[*]")
+    // enable hbase support??
 
-  // start spark session
-  val spark = new SparkSession.Builder()
-    .config(conf)
-    .getOrCreate()
+    // start spark session
+    val spark = new SparkSession.Builder()
+      .config(conf)
+      .getOrCreate()
 
-  val stream = spark
-    .readStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("subscribe", "topicName")
-    .load()
+    import spark.implicits._
 
-  stream.selectExpr("CAST(key as STRING)", "CAST(key as STRING)")
-    .as[(String, String)]
+    val stream = spark.readStream
+      .format(source = "kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "covid_v1")
+      .load()
+
+    val stream2 = stream.selectExpr("CAST(value AS STRING)")
+
+    val query = stream2.writeStream
+      .format("console")
+      .start()
+
+
+    query.awaitTermination()
+
+  }
 
   // stream.writeStream
   // to Hbase? need to import Hbase connection
